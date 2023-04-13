b2e50aff7fcec954619e0778f22b3f51d97d55c5
LukeWebsterKhan <lukewebsterkhan@gmail.com>
2023-04-13 11:53:38 +0100

    fix: removed comment

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -6,7 +6,6 @@ const ProjectsPage = () => {
     return(
         <>
         <h1>Projects</h1>
-        {/* {JSON.stringify(MOCK_PROJECTS, null, '\t')} */}
         <ProjectList projects={MOCK_PROJECTS} />
         </>
     )
