4dc49257853ce6198751499f1f6b14b934522bf9
LukeWebsterKhan <lukewebsterkhan@gmail.com>
2023-04-13 16:58:04 +0100

    feat: added validation

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -14,13 +14,19 @@ const Projectform = ({
   onSave,
 }: ProjectFormProps) => {
   const [project, setProject] = useState(initalProject);
+  const [errors, setErrors] = useState({
+    name: "",
+    description: "",
+    budget: "",
+  });
   const handleSubmit = (event: SyntheticEvent) => {
     event.preventDefault();
+    if (!isValid()) return;
     onSave(project as Project);
   };
   const handleChange = (event: SyntheticEvent) => {
     const { type, name, value, checked } = event.target as HTMLInputElement;
-    let updatedValue : any = type === "checkbox" ? checked : value;
+    let updatedValue: any = type === "checkbox" ? checked : value;
     if (type === "number") {
       updatedValue = Number(updatedValue);
     }
@@ -32,6 +38,30 @@ const Projectform = ({
       updatedProject = new Project({ ...p, ...change });
       return updatedProject;
     });
+    setErrors(() => validate(updatedProject));
+  };
+  const validate = (project: Project) => {
+    let errors = { name: "", description: "", budget: "" };
+    if (project.name.length === 0) {
+      errors.name = "Name is required";
+    }
+    if (project.name.length > 0 && project.name.length < 3) {
+      errors.name = "Name must be at least 3 characters";
+    }
+    if (project.description.length === 0) {
+      errors.description = "Description is required";
+    }
+    if (project.budget <= 0) {
+      errors.budget = "Budget must be greater than 0";
+    }
+    return errors;
+  };
+  const isValid = () => {
+    return (
+      errors.name.length === 0 &&
+      errors.description.length === 0 &&
+      errors.budget.length === 0
+    );
   };
   return (
     <form className="input-group vertical" onSubmit={handleSubmit}>
@@ -43,6 +73,11 @@ const Projectform = ({
         onChange={handleChange}
         value={project?.name}
       />
+      {errors.name.length > 0 && (
+        <div className="card error">
+          <p>{errors.name}</p>
+        </div>
+      )}
       <label htmlFor="description">Project Description</label>
 
       <textarea
@@ -51,6 +86,11 @@ const Projectform = ({
         onChange={handleChange}
         value={project?.description}
       ></textarea>
+      {errors.description.length > 0 && (
+        <div className="card error">
+          <p>{errors.description}</p>
+        </div>
+      )}
       <label htmlFor="budget">Project Budget</label>
 
       <input
@@ -60,8 +100,18 @@ const Projectform = ({
         onChange={handleChange}
         value={project?.budget}
       />
+      {errors.budget.length > 0 && (
+        <div className="card error">
+          <p>{errors.budget}</p>
+        </div>
+      )}
       <label htmlFor="isActive">Active?</label>
-      <input type="checkbox" name="isActive" checked={project?.isActive} onChange={handleChange} />
+      <input
+        type="checkbox"
+        name="isActive"
+        checked={project?.isActive}
+        onChange={handleChange}
+      />
 
       <div className="input-group">
         <button className="primary bordered medium">Save</button>
