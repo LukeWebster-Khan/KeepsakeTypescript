e2142c3ff5965adcec2edb322be72bb21805cadb
LukeWebsterKhan <lukewebsterkhan@gmail.com>
2023-04-13 10:39:57 +0100

    feat: displayed list data

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,21 +1,30 @@
-import propTypes from 'prop-types';
+import propTypes from "prop-types";
+// import Project from './MockProjects'
 
-const ProjectList = ({projects}: any) => {
-    return(
-        <>
-        {/* <pre>
-        {JSON.stringify(projects, null, '\t')}
-        </pre> */}
-        <ul>
-            {projects.map((project : any) => (
-                <li key={project.id}>{project.name}</li>
-            ))}
-        </ul>
-        </>
-    );
-}
+const ProjectList = ({ projects }: any) => {
+  return (
+    <>
+    <div className="row">
+     {projects.map((project: any) => (
+        <div key={project.id} className="cols-sm">
+          <div className="card">
+            <img src={project.imageUrl} alt={project.name} />
+            <section className="section dark">
+              <h5 className="strong">
+                <strong>{project.name}</strong>
+              </h5>
+              <p>{project.description}</p>
+              <p>Budget : £{project.budget.toLocaleString()}</p>
+            </section>
+          </div>
+        </div>
+      ))}
+    </div>
+    </>
+  );
+};
 
 ProjectList.propTypes = {
-    projects: propTypes.array
-}
+  projects: propTypes.array,
+};
 export default ProjectList;
