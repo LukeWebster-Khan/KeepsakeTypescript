6dcd9b9a4ed37807f714cd67a06fc2260ff19eb6
LukeWebsterKhan <lukewebsterkhan@gmail.com>
2023-04-13 10:39:41 +0100

    fix: remove greeter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import ProjectsPage from './projects/ProjectsPage';
-import Greeter from './projects/Greeter';
 
 function App() {
 
   return (
     <div className='container'>
     <ProjectsPage />
-    <Greeter first='John' />
     </div>
   );
 }
