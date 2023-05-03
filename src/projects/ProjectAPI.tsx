8b52480dd24908c0f9b121c1982983c707736bf3
LukeWebsterKhan <lukewebsterkhan@gmail.com>
2023-05-03 11:55:18 +0100

    feat: add find method to get single project

--- src/projects/ProjectAPI.tsx
+++ src/projects/ProjectAPI.tsx
@@ -50,6 +50,13 @@ function convertToProjectModel(item: any): Project {
 }
 
 const projectAPI = {
+  find(id: number) {
+    return fetch(`${url}/${id}`)
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(convertToProjectModel);
+  },
+
   put(project: Project) {
     return fetch(`${url}/${project.id}`, {
       method: 'PUT',
