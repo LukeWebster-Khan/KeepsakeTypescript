34a048ccfd56897c9b4d58b795b4065dfb519586
LukeWebsterKhan <lukewebsterkhan@gmail.com>
2023-05-03 11:30:01 +0100

    feat: Use React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import ProjectsPage from './projects/ProjectsPage';
+import { BrowserRouter as Router, Routes, Route, NavLink} from 'react-router-dom';
+import HomePage from './home/HomePage';
 
-function App() {
-
+const App = () => {
   return (
-    <div className='container'>
-    <ProjectsPage />
-    </div>
+    <Router>
+      <header className='sticky'>
+        <span className='logo'>
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99"  />
+        </span>
+        <NavLink className='button rounded' to="/">
+        <span className="icon-home"></span>Home</NavLink>
+        <NavLink className='button rounded' to="/projects">
+        <span className="icon-briefcase"></span>Projects</NavLink>
+        
+      </header>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/projects' element={<ProjectsPage />} />
+          </Routes>
+        </div>
+    </Router>
   );
 }
 
